c1de17f0a9b86cbc8007eb24d23f9c95d672a932
BatranuVali <valentinemsphotography@gmail.com>
2024-01-01 14:06:08 +0200

    Add roots object\ structure

---
+++ BookmarkManager/Rootobject.cs
@@ -0,0 +1,9 @@
+﻿namespace BookmarkManager
+{
+    internal class Rootobject
+    {
+        internal string checksum { get; set; }
+        internal Roots roots { get; set; }
+        internal int version { get; set; }
+    }
+}
--- BookmarkManager/URL.cs
+++ BookmarkManager/URL.cs
@@ -6,7 +6,6 @@
         public string date_last_used { get; set; }
         public string guid { get; set; }
         public string id { get; set; }
-        public MetaData meta_info { get; set; }
         public string name { get; set; }
         public string type { get; set; }
         public string url { get; set; }
